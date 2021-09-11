a5c0e4b27aa228c70b597ade9ce07946d0d5624a
TheRealJan <jan.m.tennert@gmail.com>
2021-09-12 00:41:25 +0200

    Add maven central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,9 @@ plugins {
     kotlin("multiplatform") version "1.5.30"
     kotlin("plugin.serialization") version "1.5.30"
     id("maven-publish")
-
-
+    signing
+    id("org.jetbrains.dokka") version "1.4.20"
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
 }
 
 group = "io.github.jan.discordkm"
@@ -13,18 +14,98 @@ repositories {
     mavenCentral()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = group.toString()
-            artifactId = "DiscordKM"
-            version = version
+val javadocJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Javadoc JAR"
+    archiveClassifier.set("javadoc")
+    from(tasks.named("dokkaHtml"))
+}
 
-            from(components["kotlin"])
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri(Meta.release))
+            snapshotRepositoryUrl.set(uri(Meta.snapshot))
+            val ossrhUsername = providers
+                .environmentVariable("OSSRH_USERNAME")
+                .forUseAtConfigurationTime()
+            val ossrhPassword = providers
+                .environmentVariable("OSSRH_PASSWORD")
+                .forUseAtConfigurationTime()
+            if (ossrhUsername.isPresent && ossrhPassword.isPresent) {
+                username.set(ossrhUsername.get())
+                password.set(ossrhPassword.get())
+            }
         }
     }
 }
 
+object Meta {
+    const val desc = "A Kotlin Multiplatform Discord API Wrapper "
+    const val license = "GNU General Public License v3.0"
+    const val githubRepo = "jan-tennert/DiscordKM"
+    const val release = "https://s01.oss.sonatype.org/service/local/"
+    const val snapshot = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["kotlin"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+            pom {
+                name.set(project.name)
+                description.set(Meta.desc)
+                url.set("https://github.com/${Meta.githubRepo}")
+                licenses {
+                    license {
+                        name.set(Meta.license)
+                        url.set("https://www.gnu.org/licenses/gpl-3.0.html")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("TheRealJanGER")
+                        name.set("Jan Tennert")
+                        organization.set("Jan Tennert")
+                        organizationUrl.set("https://developers.redhat.com/")
+                    }
+                }
+                scm {
+                    url.set(
+                        "https://github.com/jan-tennert"
+                    )
+                    connection.set(
+                        "scm:git:git://github.com/${Meta.githubRepo}.git"
+                    )
+                    developerConnection.set(
+                        "scm:git:git://github.com/${Meta.githubRepo}.git"
+                    )
+                }
+                issueManagement {
+                    url.set("https://github.com/${Meta.githubRepo}/issues")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey = providers
+        .environmentVariable("GPG_SIGNING_KEY")
+        .forUseAtConfigurationTime()
+    val signingPassphrase = providers
+        .environmentVariable("GPG_SIGNING_PASSPHRASE")
+        .forUseAtConfigurationTime()
+    if (signingKey.isPresent && signingPassphrase.isPresent) {
+    useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
+    val extension = extensions
+        .getByName("publishing") as PublishingExtension
+    sign(extension.publications)
+}
+}
 
 kotlin {
     jvm {
--- src/commonMain/kotlin/io/github/jan/discordkm/entities/guild/invites/Invite.kt
+++ src/commonMain/kotlin/io/github/jan/discordkm/entities/guild/invites/Invite.kt
@@ -1,3 +1,12 @@
+/**
+ * DiscordKM is a kotlin multiplatform Discord API Wrapper
+ * Copyright (C) 2021 Jan Tennert
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+ */
 package io.github.jan.discordkm.entities.guild.invites
 
 import com.soywiz.klock.DateTimeTz
