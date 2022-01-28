2fa7c6ec1a61cda2d8cb1438863f68c86ea89e61
TheRealJan <jan.m.tennert@gmail.com>
2022-01-28 16:46:21 +0100

    Add command executor

---
+++ src/jvmMain/kotlin/io/github/jan/discordkm/internal/utils/commands.kt
@@ -0,0 +1,36 @@
+package io.github.jan.discordkm.internal.utils
+
+import com.google.common.reflect.ClassPath
+import com.soywiz.korio.async.invokeSuspend
+import io.github.jan.discordkm.api.entities.clients.DiscordWebSocketClient
+import io.github.jan.discordkm.api.events.CommandEvent
+import io.github.jan.discordkm.api.events.EventListener
+import io.github.jan.discordkm.api.events.SlashCommandEvent
+import java.lang.reflect.Modifier
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class CommandExecutor(
+    val name: String,
+    val subCommand: String = "",
+    val subCommandGroup: String = "",
+)
+
+fun DiscordWebSocketClient.importCommands(packageName: String, subPackages: Boolean = false) {
+    ClassPath.from(ClassLoader.getSystemClassLoader()).allClasses.filter { it.packageName == packageName || (subPackages && it.packageName.startsWith(packageName)) }.map(
+        ClassPath.ClassInfo::load).forEach {
+        it.methods.filter { m -> Modifier.isStatic(m.modifiers) && m.isAnnotationPresent(CommandExecutor::class.java) && m.parameterCount == 2 }.forEach { method ->
+            val annotation = method.getAnnotation(CommandExecutor::class.java)
+            println("ello")
+            eventListeners += EventListener { e ->
+                if(e is CommandEvent) {
+                    if(e.commandName != annotation.name) return@EventListener
+                    if(e is SlashCommandEvent) {
+                        if (annotation.subCommand.isNotBlank() && e.subCommand != annotation.subCommand) return@EventListener
+                        if (annotation.subCommandGroup.isNotBlank() && e.subCommandGroup != annotation.subCommandGroup) return@EventListener
+                    }
+                    method.invokeSuspend(null, listOf(e))
+                }
+            }
+        }
+    }
+}
--- src/jvmMain/kotlin/io/github/jan/discordkm/internal/utils/events.kt
+++ src/jvmMain/kotlin/io/github/jan/discordkm/internal/utils/events.kt
@@ -6,11 +6,12 @@ import io.github.jan.discordkm.api.entities.clients.DiscordWebSocketClient
 import io.github.jan.discordkm.api.events.EventListener
 import java.lang.reflect.Modifier
 
+@Target(AnnotationTarget.FUNCTION)
 annotation class EventListener
 
-fun DiscordWebSocketClient.loadListenersFromPackage(packageName: String) {
-    ClassPath.from(ClassLoader.getSystemClassLoader()).allClasses.filter { it.packageName == packageName }.map(ClassPath.ClassInfo::load).forEach {
-        it.methods.filter { m -> Modifier.isStatic(m.modifiers) && m.annotations.contains(EventListener()) && m.parameterCount == 2 }.forEach { method ->
+fun DiscordWebSocketClient.importListeners(packageName: String, subPackages: Boolean = false) {
+    ClassPath.from(ClassLoader.getSystemClassLoader()).allClasses.filter { it.packageName == packageName || (subPackages && it.packageName.startsWith(packageName)) }.map(ClassPath.ClassInfo::load).forEach {
+        it.methods.filter { m -> Modifier.isStatic(m.modifiers) && m.isAnnotationPresent(io.github.jan.discordkm.internal.utils.EventListener::class.java) && m.parameterCount == 2 }.forEach { method ->
             val event = method.parameterTypes[0]
             eventListeners += EventListener { e ->
                 if(event.isInstance(e)) {
